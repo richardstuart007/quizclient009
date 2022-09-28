dcf21924c037ec8fd52ba53174ef97b307ad6479
Richard <richardstuart007@hotmail.com>
2022-09-29 11:37:48 +1300

    splash to version 010

--- src/pages/QuizSplash/QuizSplash.js
+++ src/pages/QuizSplash/QuizSplash.js
@@ -1,71 +1,48 @@
-//
-//  Libraries
-//
-import { useSnapshot } from 'valtio'
 import { Container, Grid, Typography } from '@mui/material'
 //
 //  Debug Settings
 //
 import debugSettings from '../../debug/debugSettings'
-//
-//  Controls
-//
-import MyButton from '../../components/controls/MyButton'
-//
-//  Utilities
-//
-import { ValtioStore } from '../ValtioStore'
 //..............................................................................
 //.  Initialisation
 //.............................................................................
 //
 // Debug Settings
 //
-const g_log1 = debugSettings()
+const debugLog = debugSettings()
 //===================================================================================
 const QuizSplash = () => {
-  if (g_log1) console.log('Start QuizSplash')
-  //
-  //  Define the ValtioStore
-  //
-  const snapShot = useSnapshot(ValtioStore)
-  const CurrentPage = snapShot.v_Page
+  if (debugLog) console.log('Start QuizSplash')
   //...................................................................................
   //.  Render the form
   //...................................................................................
   return (
     <Grid container>
       <Container>
+        <Typography variant='h6' sx={{ marginTop: '8px', color: 'blue' }}>
+          STATIC VERSION 009
+        </Typography>
         <Typography variant='h6' sx={{ marginTop: '8px' }}>
-          This product is in Trial/Development.
+          This version has beeN DISCONTINUED
         </Typography>
         <Typography variant='subtitle2' sx={{ marginTop: '8px' }}>
           It has been developed by Richard Stuart and is FREE to use/distribute.
         </Typography>
         <Typography variant='subtitle2' sx={{ marginTop: '8px' }}>
-          If your URL does not work copy the text below into your browser,
-          alternatively email me.
+          Comments you can email me at richardstuart007@hotmail.com
+        </Typography>
+        <Typography variant='subtitle2' sx={{ marginTop: '32px', color: 'red' }}>
+          PLEASE CLICK ON ONE OF THE LINKS BELOW TO SWITCH TO THE NEW VERSION 010
+        </Typography>
+        <Typography variant='h3' sx={{ marginTop: '8px' }}>
+          <a href='https://richardstuart007.github.io/quizclient010/'>Database 010</a>
+        </Typography>
+        <Typography variant='subtitle2' sx={{ marginTop: '32px' }}>
+          Alternatively click on the Static version 010 which corresponds to version 009
         </Typography>
         <Typography variant='subtitle2' sx={{ marginTop: '8px' }}>
-          https://richardstuart007.github.io/quizclient009/
+          <a href='https://richardstuart007.github.io/quizclientstatic010/'>Static 010</a>
         </Typography>
-
-        <Typography variant='h6' sx={{ marginTop: '8px' }}>
-          Email: richardstuart007@hotmail.com
-        </Typography>
-
-        {/*.................................................................................................*/}
-        <Grid item xs={12}>
-          <MyButton
-            type='submit'
-            text='Accept'
-            value='Submit'
-            onClick={() => {
-              ValtioStore.v_PagePrevious = CurrentPage
-              ValtioStore.v_Page = 'QuizRestart'
-            }}
-          />
-        </Grid>
       </Container>
     </Grid>
   )
