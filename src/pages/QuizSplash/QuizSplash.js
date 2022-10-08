bd4d0525347c29ca1d2360b974372b7e0692e289
Richard <richardstuart007@hotmail.com>
2022-10-09 12:57:56 +1300

    splash

--- src/pages/QuizSplash/QuizSplash.js
+++ src/pages/QuizSplash/QuizSplash.js
@@ -32,17 +32,17 @@ const QuizSplash = () => {
           Comments you can email me at richardstuart007@hotmail.com
         </Typography>
         <Typography variant='subtitle2' sx={{ marginTop: '32px', color: 'red' }}>
-          PLEASE CLICK ON ONE OF THE LINKS BELOW TO SWITCH TO THE NEW VERSION 010
+          PLEASE CLICK ON THE LINK BELOW TO SWITCH TO THE NEW VERSION 010
         </Typography>
         <Typography variant='h3' sx={{ marginTop: '8px' }}>
           <a href='https://richardstuart007.github.io/quizclient010/'>Database 010</a>
         </Typography>
-        <Typography variant='subtitle2' sx={{ marginTop: '32px' }}>
+        {/* <Typography variant='subtitle2' sx={{ marginTop: '32px' }}>
           Alternatively click on the Static version 010 which corresponds to version 009
         </Typography>
         <Typography variant='subtitle2' sx={{ marginTop: '8px' }}>
           <a href='https://richardstuart007.github.io/quizclientstatic010/'>Static 010</a>
-        </Typography>
+        </Typography> */}
       </Container>
     </Grid>
   )
